a6c4da3bc710a5d7ff5cc8c3639315e0b3447373
wwoah <phillipkbishop@gmail.com>
2024-11-30 14:28:27 -0800

    Created general structure of page

---
+++ midasSite/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Button() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </>
+  );
+}
